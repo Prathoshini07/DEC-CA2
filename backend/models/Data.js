44db4ba71304bd1f2785ea6e480fc2679c8661d1
Prathoshini07 <prathoshini07@gmail.com>
2025-03-03 23:00:24 +0530

    removed files

--- backend/models/Data.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const DataSchema = new mongoose.Schema({
-    id: Number,
-    title: String,
-    body: String,
-});
-
-module.exports = mongoose.model('Data', DataSchema);
