b65409758f41b5cd92089650d54a1cac6251f5cc
Prathoshini07 <prathoshini07@gmail.com>
2025-03-03 23:24:16 +0530

    render url added

--- frontend/client/src/services/api.js
+++ frontend/client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api', // Backend API base URL
+    baseURL: 'https://dec-ca2.onrender.com/api', // Backend API base URL
 });
 
 export const fetchData = () => api.get('/fetch-data');
